349750e3f4888b4a17dad52118c710a0e5553982
Sachin Nishal <46395550+SachinNishal@users.noreply.github.com>
2020-08-15 08:17:16 +0530

    Initial Commit

---
+++ replacePreservingCaseOfReplacedwordFirstLetter/replacePreservingCaseOfReplacedwordFirstLetter.js
@@ -0,0 +1,21 @@
+function isUpperCase(word) 
+{
+    return word === word.toUpperCase();
+}
+
+function replacePreservingCaseOfReplacedwordFirstLetter(str, before, after) 
+{
+  let tempStr=isUpperCase([...before][0]);
+  if (tempStr===true)
+  {
+      let tempAfter=[...after];
+      tempAfter[0]=tempAfter[0].toUpperCase();
+      let newAfter=tempAfter.join("");
+      return str.replace(before,newAfter);
+  }
+  else
+  {
+      let newAfter=after.toLowerCase();
+      return str.replace(before,newAfter);
+  }
+}
